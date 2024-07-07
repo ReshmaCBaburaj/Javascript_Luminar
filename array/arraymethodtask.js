63979eafff8967a85102b100b5afa853e05cb71e
Reshma C Baburaj <reshmacbaburaj@gmail.com>
2024-07-07 18:40:23 +0530

    arrayemthodtask added

--- array/arraymethodtask.js
+++ array/arraymethodtask.js
@@ -25,10 +25,27 @@ console.log("\ndistrict having lowest death rate");
 console.log(covid_data.reduce((item1,item2)=>item1[3]<item2[3]?item1:item2));
 
 //4. Sort the data with +ve case in desending order
+console.log("\nSort the data with +ve case in desending order");
 
-
+console.log(covid_data.sort((pos1,pos2)=>pos2[5]-pos1[5]));
 
 //5. sort district with 1st dose vaccine
+console.log("\nsort district with 1st dose vaccine");
+covid_data.sort((item1,item2)=>item1[5]-item2[5]).forEach(dist => {
+    console.log(dist[1]);
+});
+
 //6. print total number of curred cases
+console.log("total number of curred cases");
+
+total=covid_data.map(item=>item[4]).reduce((cur1,cur2)=>cur1+cur2)
+console.log(total);
 //7. print total curred cases in Edukki
+
+console.log("total curred cases in Idukki");
+covid_data.filter(item=>item[1]=="Idukki").forEach(item1=>console.log(item1[4]));
+
 //8. Is any district having more than 27000 +ve cases -
+
+console.log("district having more than 27000 +ve cases");
+console.log(covid_data.some(item=>item[2]>27000));
